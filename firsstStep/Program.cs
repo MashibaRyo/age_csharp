9a22951165741f854d5d513c62be0a7547d863d2
Umarov Gadzimurad <dostoyanie1977yndex.com@gmail.com>
2024-09-10 16:04:04 +0300

    first commit

---
+++ firsstStep/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите ваш возраст: ");
+
+        int age;
+        while (!int.TryParse(Console.ReadLine
+            (), out age))
+        {
+            Console.Write("Введите число пожалуйста: ");
+        }
+        if (age < 18)
+        {
+            Console.WriteLine($"Приходите через {18 - age} лет");
+        }
+        else
+        {
+            Process.Start("https://www.youtube.com/watch?v=w8rRhAup4kg&t=11s");
+            Console.WriteLine("Ссылка открыта в браузере");
+        }
+
+    }
+}
